97ee494fa68a314160ab84104ad9e89288e6e3da
Cem Zarifoğlu <cem0509@hotmail.com>
2021-04-24 17:45:53 +0300

    Added Player Interaction.

---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+class PlayerInteraction : MonoBehaviour {
+    public float Range = 2f;
+    public UnityEvent OnActivation;
+    public UnityEvent<string, bool> OnHoverObject;
+
+    public void Update() {
+        Ray ray = new Ray(this.transform.position, this.transform.forward);
+        if(Physics.Raycast(ray, out var hit, this.Range)) {
+            var AObj = hit.collider.GetComponent<ActivatableObject>();
+
+            if(AObj) {
+                OnHoverObject.Invoke(AObj.ObjectName, AObj.Activateable);
+            }
+        }
+    }
+
+    [ContextMenu("RayCastInteract")]
+    public void RayCastInteract() {
+        if(Physics.Raycast(this.transform.position, this.transform.forward, out var hit, this.Range)) {
+            var AObj = hit.collider.GetComponent<ActivatableObject>();
+
+            if(AObj != null && AObj.Activateable) {
+                OnActivation.Invoke();
+                AObj.Activate();
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.forward * this.Range);
+    }
+}
