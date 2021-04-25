e1abc26939fa16c3b49c11c0b10b932cd5a0b4fd
MustachedNinja <kostya.yatsuk9@live.com>
2021-04-25 02:17:12 -0700

    Reorganize and add some barebones rooms

--- Assets/Scripts/Objects/Projectile.cs
+++ Assets/Scripts/Objects/Projectile.cs
@@ -4,11 +4,22 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float projectileSpeed = 25f;
     Rigidbody rb;
+
+    private float lifeTime = 4f;
+    private float timeAlive;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * projectileSpeed;
+        timeAlive = 0f;
+    }
+
+    void Update() {
+        timeAlive += Time.deltaTime;
+        if (timeAlive > lifeTime) {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter(Collision collision) {
